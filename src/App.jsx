de7850d3ce824a02406c31b7d4d8a9a5dfa1fadd
Advait Variyar <advaitvariyar7@gmail.com>
2022-08-07 12:05:48 +0530

    Customise Project Structure
    Delete Create React App Files and Add Custom Project Structure with Mock Home Page

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import { Home } from './exporter'
+
+export default function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="home">
+      <div className="home-title-box">
+        <h1 className="title">PlanIt</h1>
+        <h2 className="sub-title">The Responsive Automatic Scheduler Made for You</h2>
+      </div>
+    </div>
+  )
+}
